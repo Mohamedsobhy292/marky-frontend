f334e3ed1cadbf55d39433a5bfc1b27897d3deb0
Mohamed sobhy <mohamed sobhy>
2017-11-23 14:11:18 +0200

    adding eslint rules and fixing eslint errors

--- src/app/app.js
+++ src/app/app.js
@@ -1,15 +1,11 @@
-import React, { Component } from 'react';
-import Navbar from './navbar'
+import React from 'react';
+import Navbar from './navbar';
 import './app.scss';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Navbar />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <Navbar />
+  </div>
+);
 
 export default App;
--- src/app/navbar.js
+++ src/app/navbar.js
@@ -1,30 +1,30 @@
 import React, { Component } from 'react';
 import logo from '../assets/img/logo.svg';
 
-export default class  extends Component {
+export default class extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      loggedIn: false
+      loggedIn: false,
     };
   }
   render() {
-    const {loggedIn} = this.state.loggedIn;
+    const { loggedIn } = this.state.loggedIn;
     return (
       <nav className="main-nav">
-          <ul className="main-nav__list left-list">
-            <li className="main-nav__link">Boards</li>
-          </ul>
-          <div className="main-nav__list">
-            <img src={logo} alt="" height="50" />
-          </div>
-          {loggedIn ?
+        <ul className="main-nav__list left-list">
+          <li className="main-nav__link">Boards</li>
+        </ul>
+        <div className="main-nav__list">
+          <img src={logo} alt="" height="50" />
+        </div>
+        {loggedIn ?
           <ul className="main-nav__list right-list">
             <li className="main-nav__link">Mohamed Sobhy</li>
             <li className="main-nav__link">
               <button className="primary-btn">New</button>
             </li>
-          </ul>:
+          </ul> :
           <ul className="main-nav__list right-list">
             <li className="main-nav__link">Sign Up</li>
             <li className="main-nav__link">
