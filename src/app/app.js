ef02ffd8ac2c43bb435cc24a91eff2de0d218d49
Mohamed sobhy <mohamedsobhy292@gmail.com>
2017-11-24 17:38:07 +0200

    sidebar, added roboto bold

--- src/app/app.js
+++ src/app/app.js
@@ -3,11 +3,28 @@ import Navbar from './navbar';
 import Sidebar from './sidebar';
 import './app.scss';
 
+const data = [
+  {
+    name: 'Project Board',
+    id: 50,
+    color: '#D45D79',
+  },
+  {
+    name: 'Work Board',
+    id: 55,
+    color: '#F9B248',
+  },
+  {
+    name: 'mido3',
+    id: 60,
+    color: '#C4AFF0',
+  },
+];
 const App = () => (
   <div className="App">
     <Navbar loggedIn />
     <div className="main-layout">
-      <Sidebar />
+      <Sidebar boardsList={data} />
       <div className="main-content">www</div>
     </div>
   </div>
--- src/app/sidebar.js
+++ src/app/sidebar.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+function setBgStyle(itembackround) {
+  return {
+    background: itembackround,
+  };
+}
+
 const Sidebar = ({boardsList}) => (
   <div className="main-sidebar">
-    <h2>Your Boards</h2>
-    <ul>
+    <h5 className="main-sidebar__title">Your Boards</h5>
+    <ul className="main-sidebar__boards">
       {boardsList.map(item => (
-        <li>
-          <span>{item.name}</span>
+        <li className="main-sidebar__list-item">
+          <span className="list-item-icon" style={setBgStyle(item.color)} />
+          <a href={item.id}>{item.name}</a>
         </li>
         ))}
     </ul>
+    <button className="secondary-btn new-board-btn">New Board </button>
   </div>
 );
 
 Sidebar.defaultProps = {
-  boardsList: [{name: 'beebo'}],
+  boardsList: [],
 };
 
 Sidebar.propTypes = {
