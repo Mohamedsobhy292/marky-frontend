f22b5495e8df8f066c872ee90f5b0153f186fccd
Mohamed sobhy <mohamedsobhy292@gmail.com>
2017-11-25 17:11:20 +0200

    modifing board component style

--- src/board/Board.js
+++ src/board/Board.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './board.scss';
+import home from '../assets/img/home.svg';
 
 function setBgStyle(color) {
   return {
-    borderColor: color,
+    backgroundColor: color,
   };
 }
 
 const Board = ({boardData}) => (
-  <div className="board-component" style={setBgStyle(boardData.color)}>
-    <h3 className="board-component__title">
-      <a href={boardData.id}>{boardData.name}</a>
-    </h3>
-  </div>
+  <a className="board-component" href={boardData.id}>
+    <div className="board-component__icon" style={setBgStyle(boardData.color)}>
+      <img src={home} alt="" width="30" />
+    </div>
+    <div className="board-component__content">
+      <h3 className="board-component__title">
+        <a href={boardData.id}>{boardData.name}</a>
+      </h3>
+    </div>
+  </a>
 );
 
 Board.propTypes = {
